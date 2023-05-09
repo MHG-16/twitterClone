fa7aa97c27fd5b3aa9d36740f3171ee329d8dae4
mhg-16 <hammahadi2006@gmail.com>
2023-05-09 18:22:11 +0200

    add formTweet

---
+++ app/components/form/Form.tsx
@@ -0,0 +1,108 @@
+'use client';
+
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+import { Post, User } from '@prisma/client';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+import React, { useCallback, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import Button from '../share/Button';
+import Avatar from '../share/Avatar';
+
+
+interface FormProps {
+  placeholder: string;
+  isComment?: boolean;
+  postId?: string;
+  posts: Post[];
+  currentUser: {currentUser: User} | null
+}
+const Form : React.FC<FormProps> = ({
+  placeholder,
+  isComment,
+  postId,
+  currentUser
+}) => {
+
+  
+  return (
+    <div className='border-b-[1px] border-neutral-800 px-5 py-2'>
+      {currentUser ? <WhenUserConnected 
+        user={currentUser.currentUser}  placeholder={placeholder}
+      /> : <HeaderWhenUserNotConnected />}
+    </div>
+  )
+}
+
+
+const HeaderWhenUserNotConnected = () => {
+  const registerModal = useRegisterModal();
+  const loginModal = useRegisterModal();
+  return (
+    <div className='py-8'>
+        <h1 className='text-white text-2xl 
+        text-center mb-4 font-bold'>
+          Welcome to Twitter
+        </h1>
+        <div className='flex flex-row items-center justify-center gap-4'>
+          <Button label='Login' onClick={loginModal.onOpen}/>
+          <Button label='Register' onClick={registerModal.onOpen}/>
+        </div>
+      </div>
+  )
+}
+
+interface WhenUserConnectedProps{
+  user: User,
+  placeholder: string
+}
+const WhenUserConnected : React.FC<WhenUserConnectedProps>= ({
+  user : currentUser,
+  placeholder
+}) => {
+  const [body, setBody] = useState('');
+  const [isLoading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const onSubmit = useCallback(async () => {
+    try{
+      setLoading(true);
+
+      await axios.post('/api/post/', { body });
+
+      setBody('');
+      toast.success("Tweet created");
+      router.refresh();
+    }catch(error){
+      toast.error("Something went wrong!")
+    } finally {
+      setLoading(false);
+    }
+  }, [body, router]);
+  
+  return (
+    <div className={"flex flex-row gap-4"}>
+      <div>
+        <Avatar user={currentUser}/>
+      </div>
+      <div className='w-full'>
+        <textarea disabled={isLoading}
+          onChange={(e) => setBody(e.target.value)}
+          value={body}
+          className='disabled:opacity-80 peer resize-none
+          mt-3 w-full bg-black ring-0 outline-none text-lg
+          placeholder-neutral-500 text-white' 
+          placeholder={placeholder}
+        ></textarea>
+        <hr 
+          className='opacity-0 peer-focus:opacity-100
+          h-[1px] w-full border-neutal-800 transition'
+        />
+        <div className='mt-4 flex flex-row justify-end'>
+          <Button label='Tweet' onClick={onSubmit}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Form
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getPosts from "./actions/getPosts";
+import Form from "./components/form/Form";
 import Header from "./components/share/Header";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts({userId: ''});
+  const user = await getCurrentUser();
   return (
     <>
       <Header label="Home" />
+      <Form placeholder="What's happening?" 
+      posts={posts} currentUser={user}/>
     </>
   )
 }
